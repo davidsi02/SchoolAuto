79dc9d11704c4b6e666d1923d6b994920d09f5cc
davidsi02 <49164276+davidsi02@users.noreply.github.com>
2020-03-24 12:14:03 +0000

    *Otimizado

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -17,108 +17,48 @@ class PermissionController extends Controller
 
      @csfr;
 
-  $verificaPermAdm = DB::table('permission')->
+     session_start();
+
+     if (Auth::user()->tipoUtilizador == 1){
+
+         $_SESSION['permAdmin'] = 1;
+         $_SESSION['permSA'] = 1;
+
+          if ($_SESSION['permAdmin'] == 1) {
+           $_SESSION['permSA'] == 1;
+         }
+
+        if($_SESSION['permSA'] == 1){
+                 $_SESSION['AcessoCantina'] = 1;
+                 $_SESSION['AcessoPortaria'] = 1;
+                 $_SESSION['AcessoBar'] = 1;
+                 $_SESSION['AcessoBiblioteca'] = 1;
+        } else {
+
+  $_SESSION['permAdmin'] = DB::table('permission')->
   where('idUser', Auth::user()->id) ->
   value('Admin');
 
-  $verificaPermSA = DB::table('permission')->
+  $_SESSION['permSA'] = DB::table('permission')->
   where('idUser', Auth::user()->id) ->
   value('SAE');
 
-  $verificaAcessoBar = DB::table('permission')->
+  $_SESSION['AcessoBar'] = DB::table('permission')->
   where ('idUser', Auth::user()->id) ->
   value('AcessoBar');
 
-  $verificaAcessoCantina = DB::table('permission')->
+  $_SESSION['AcessoCantina'] = DB::table('permission')->
   where('idUser', Auth::user()->id) ->
   value('AcessoCantina');
 
-  $verificaAcessoBiblioteca =  DB::table('permission')->
+  $_SESSION['AcessoBiblioteca'] =  DB::table('permission')->
   where('idUser', Auth::user()->id) ->
   value('AcessoBiblioteca');
 
-  $verificaAcessoPortaria =  DB::table('permission')->
+  $_SESSION['AcessoPortaria'] =  DB::table('permission')->
   where('idUser', Auth::user()->id) ->
   value('AcessoPortaria');
-
-
-
-  session_start();
-
-
-
-if (Auth::user()->tipoUtilizador == 1){
-
-    $_SESSION['permAdmin'] = 1;
-    $_SESSION['permSA'] = 1;
-
-} else {
-  switch ($verificaPermAdm) {
-      case 0:
-          $_SESSION['permAdmin'] = 0;
-          break;
-      case 1:
-          $_SESSION['permAdmin'] = 1;
-          break;
-  }
-
-
-  switch ($verificaPermSA) {
-      case 0:
-          $_SESSION['permSA'] = 0;
-          break;
-      case 1:
-          $_SESSION['permSA'] = 1;
-          break;
-  }
-
-  switch ($verificaAcessoCantina) {
-      case 0:
-          $_SESSION['AcessoCantina'] = 0;
-          break;
-      case 1:
-          $_SESSION['AcessoCantina'] = 1;
-          break;
-  }
-
-  switch ($verificaAcessoPortaria) {
-      case 0:
-          $_SESSION['AcessoPortaria'] = 0;
-          break;
-      case 1:
-          $_SESSION['AcessoPortaria'] = 1;
-          break;
-  }
-
-  switch ($verificaAcessoBar) {
-      case 0:
-          $_SESSION['AcessoBar'] = 0;
-          break;
-      case 1:
-          $_SESSION['AcessoBar'] = 1;
-          break;
-  }
-
-  switch ($verificaAcessoBiblioteca) {
-      case 0:
-          $_SESSION['AcessoBiblioteca'] = 0;
-          break;
-      case 1:
-          $_SESSION['AcessoBiblioteca'] = 1;
-          break;
-  }
-
-  if ($_SESSION['permAdmin'] == 1) {
-    $_SESSION['permSA'] == 1;
-  }
-
-  if($_SESSION['permSA'] == 1){
-            $_SESSION['AcessoCantina'] = 1;
-            $_SESSION['AcessoPortaria'] = 1;
-            $_SESSION['AcessoBar'] = 1;
-            $_SESSION['AcessoBiblioteca'] = 1;
-   }
-
+}
 
 }
 
