cedb5e8da6b7c8dab7ea6879fa8cad75ae751886
davidsi02 <49164276+davidsi02@users.noreply.github.com>
2020-03-25 10:02:03 +0000

    ---

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -18,12 +18,6 @@
 	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <meta name="viewport" content="width=device-width" />
 
-
-
-
-
-
-
 </head>
 <body>
 
