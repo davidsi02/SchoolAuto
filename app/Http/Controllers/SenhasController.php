105b33fc3de2cd81996e7a8bc87410579fab38d6
JPCalhau <jpcalhau@gmail.com>
2020-05-05 23:43:31 +0100

    dashboard.blade.php e SenhasController

--- app/Http/Controllers/SenhasController.php
+++ app/Http/Controllers/SenhasController.php
@@ -15,10 +15,39 @@ class SenhasController extends Controller
     * @return \Illuminate\Http\Response
     */
     public function showSenha(){
-
+      $ndiasmax = 7; // N.º dias que querem que "apareça"
       $currentdate = time();
       $diasemana = array('Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado');
 
+      $senha = collect([['dataRefeicao' => date("Y-m-d", time()+86400), 'diasemana' => $diasemana[date("w",  time()+86400)]]]);
+
+       $controlo = 2;
+      for ($ndias=2;$ndias<=$ndiasmax;$ndias++){
+       $dia_aux = time()+86400*$controlo;
+       $diasemana_aux = date("w", $dia_aux);
+
+      while ($diasemana_aux == 0 || $diasemana_aux == 6) {
+          $dia_aux = time()+86400*(++$controlo);
+          $diasemana_aux = date("w", $dia_aux);
+       }
+      ++$controlo;      
+      $senha->push(['dataRefeicao' => date("Y-m-d", $dia_aux), 'diasemana' => $diasemana[$diasemana_aux]]);
+      }
+
+     $senha=$senha->map(function($row) {
+        return (object) $row;
+      });
+
+/*
+$myCollection = collect([
+    ['product_id' => 1, 'price' => 200, 'discount' => '50'],
+    ['product_id' => 2, 'price' => 400, 'discount' => '50']
+])->map(function($row) {
+    return collect($row);
+});
+*/
+
+/*
      $counter = 1;
 
 
@@ -51,8 +80,10 @@ class SenhasController extends Controller
  } while ($counter < 8);
 
  echo count($senha);
+*/
 
-    return view('user/dashboard', compact('senha', ['senha' => $senha]));
+//    return view('user/dashboard', compact('senha', ['senha' => $senha]));
+    return view('user/dashboard', compact('senha', $senha));
 
     }
 
